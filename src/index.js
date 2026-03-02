c3159ab8fb088454f0f294bd1b6a2228e051fa19
Nick Rashchuk <raschyk1993@gmail.com>
2026-03-02 21:22:03 +0200

    create markup

--- src/index.js
+++ src/index.js
@@ -12,43 +12,76 @@ searchForm.addEventListener('submit', handlerForm);
 
 function handlerForm(e) {
   e.preventDefault();
-  const inputValue = e.currentTarget[0].value;
-  getPicture(inputValue);
-
-  async function getPicture(value) {
-    try {
-      const response = await axios.get(BASE_URL, {
-        params: {
-          key: API_KEY,
-          q: value,
-          image_type: 'photo',
-          orientation: 'horizontal',
-          safesearch: true,
-          page: page,
-          per_page: 40,
-        },
-      });
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
+  loadMoreBtn.classList.add('hidden');
+  gallary.innerHTML = '';
+  const inputValue = searchForm[0].value;
+  getPicture(inputValue).then(resp => {
+    const totalPictures = resp.totalHits;
+    if (totalPictures > page * 40) {
+      page++;
+      loadMoreBtn.classList.remove('hidden');
+    } else {
+      loadMoreBtn.classList.add('hidden');
     }
+    const pictures = createMarkup(resp.hits);
+    gallary.insertAdjacentHTML('beforeend', pictures);
+  });
+}
+
+async function getPicture(value) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: page,
+        per_page: 40,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
 }
 
-/* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>; */
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+        <div class="photo-card">
+          <img class="photo-img" src="${webformatURL}" alt="${tags}" loading="${largeImageURL}" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              ${downloads}
+            </p>
+          </div>
+        </div>`
+    )
+    .join('');
+}
+
+loadMoreBtn.addEventListener('click', handlerForm);
